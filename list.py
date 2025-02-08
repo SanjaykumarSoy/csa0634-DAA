0e7ff8d884c1f3f4981246bcd61d9964fda42211
SanjaykumarSoy <157505017+SanjaykumarSoy@users.noreply.github.com>
2025-02-08 13:27:38 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,8 @@
+empty_list = []
+print(empty_list)
+single_element_list = [1]
+print(single_element_list)
+identical_elements_list = [7, 7, 7, 7]
+print(identical_elements_list)
+negative_numbers_list = [-5, -1, -3, -2, -4]
+print(negative_numbers_list)
