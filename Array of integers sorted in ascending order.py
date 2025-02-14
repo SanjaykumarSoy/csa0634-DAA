6fdc397d1c904622713ad7e22f3b554a92e3e1d9
SanjaykumarSoy <157505017+SanjaykumarSoy@users.noreply.github.com>
2025-02-14 09:41:59 +0530

    Create Array of integers sorted in ascending order.py

---
+++ Array of integers sorted in ascending order.py
@@ -0,0 +1,8 @@
+
+array = [2, 4, 6, 8, 10, 12, 14, 18]
+maximum_value = max(array)
+minimum_value = min(array)
+
+# Output the results
+print("Maximum Value:", maximum_value)
+print("Minimum Value:", minimum_value)
